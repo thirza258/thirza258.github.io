ceee2b5101a16c1d0a46c5a5affb36dd4b49b092
Thirza <thirzahmad@gmail.com>
2024-07-05 22:13:52 +0700

    add something

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig,  } from 'vite'
+import { build, defineConfig,  } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
 })
 
-module.exports = {
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true, // also necessary
-  }
-}
