9d1db859ee0ca43e667feebbe6f51dad01c66506
Thirza <thirzahmad@gmail.com>
2024-07-05 22:10:33 +0700

    add

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,11 @@ module.exports = {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+      { allowNamedExport: true},
+      { allowDefaultExport: true},
+      { allowDirectExport: true},
+
     ],
+    'no-unused-vars': 'off',
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig,  } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,3 +6,11 @@ export default defineConfig({
   plugins: [react()],
   base: '/'
 })
+
+module.exports = {
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true, // also necessary
+  }
+}
